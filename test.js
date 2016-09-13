3c891573614f203e62fa34275bca6a20e3321dd6
jnusic <jnajera1917@gmail.com>
2016-09-12 20:03:39 -0400

    segment controller logic

--- test.js
+++ test.js
@@ -8,23 +8,47 @@ window.addEventListener('load', function () {
 		arr[i].onclick = segmentedLogic;
 	};
 
+	function candidateSwitchLogic() {
+
+		}
 
 
 	function segmentedLogic(element) {
 	var clickedId = element.srcElement.id;
 
+	if (clickedId == "seg3") {
+		document.getElementsByClassName("segment_icon fa fa-info fa-lg")[0].id = " ";
+		document.getElementsByClassName("segment_icon fa fa-list fa-lg")[0].id = "selected";
+		document.getElementsByClassName("segment_icon fa fa-money fa-lg")[0].id = " ";
+		document.getElementById('seg1').className = "segment";
+		document.getElementById('seg2').className = "segment";
+		element.srcElement.className += " " + "default";
+
+		document.getElementsByClassName("segment_title_text")[0].innerHTML = "Election Platform"
+	};
+
 	if (clickedId == "seg2") {
+		document.getElementsByClassName("segment_icon fa fa-info fa-lg")[0].id = "selected";
+		document.getElementsByClassName("segment_icon fa fa-list fa-lg")[0].id = " ";
+		document.getElementsByClassName("segment_icon fa fa-money fa-lg")[0].id = " ";
+		document.getElementById('seg1').className = "segment";
 		document.getElementById('seg3').className = "segment";
-		var icon = document.getElementById('seg3').firstChild.innerHTML; 
-		console.log(icon);
-		icon.id = "test";
+		element.srcElement.className += " " + "default";
+
+		document.getElementsByClassName("segment_title_text")[0].innerHTML = "General Election Info."
+	};
+
+	if (clickedId == "seg1") {
+		document.getElementsByClassName("segment_icon fa fa-money fa-lg")[0].id = "selected";
+		document.getElementsByClassName("segment_icon fa fa-list fa-lg")[0].id = " ";
+		document.getElementsByClassName("segment_icon fa fa-info fa-lg")[0].id = " ";
+		document.getElementById('seg2').className = "segment";
+		document.getElementById('seg3').className = "segment";
+		element.srcElement.className += " " + "default";
+
+		document.getElementsByClassName("segment_title_text")[0].innerHTML = "Latest Live Polls"
 	}; 
-	console.log(element.srcElement.className);
-	console.log(element.srcElement.id);
-
-	element.srcElement.className += " " + "default"; 
-
-	}
+  }
 })
 
 
