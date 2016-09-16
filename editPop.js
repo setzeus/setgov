f6b2bd8efba31330005de5a34534b3ad580ff563
Bernardo Garciarivas <brgarciarivas@gmail.com>
2016-09-16 01:57:25 -0400

    clean up

--- editPop.js
+++ editPop.js
@@ -1,19 +1,18 @@
+
+//Going to hold whats sent from the background page
  var data ;
 
+//Send message to background waiting for reponse of words matched from current visted page
 chrome.runtime.sendMessage({action: "editPop"},function(response){
-  
   data = response
   adjustPopUp(response[0], 'red');
 });  
 
 
-
- 
+//Event Listener to change profile when candiate_switch is clicked
   document.getElementById("candidate_switch").addEventListener('click', checkColor);
-  
   function checkColor(){
       var switch_color = document.getElementById("candidate_switch").className
-
      
       if (switch_color== "icon_container left red") {
         adjustPopUp(data[1], 'blue')
@@ -25,15 +24,9 @@ chrome.runtime.sendMessage({action: "editPop"},function(response){
 
 
 
-
-
-
-
-
-
-//handles the data from getKeyWordsFromPage
+//handles the data from getKeyWordsFromPage and profile change
 function adjustPopUp(sortedMatched, color){
-
+  //Set variables ready for color that should be displayed
   if(color == 'red'){
     var buttonColor = 'button-red';
     var ampersandColor = 'ampersand_color_red';
@@ -43,20 +36,11 @@ function adjustPopUp(sortedMatched, color){
     var buttonColor = 'button-blue';
     var ampersandColor = 'ampersand_color_blue';
     var rowLineColor = 'row_line_blue'
-
   }
 
+  var clearDivv = document.getElementById('electionPositions').innerHTML = ' ' 
 
-
-
-  var clearDivv = document.getElementById('electionPositions').innerHTML = ' '
-  console.log('adjust popup went off inside edit pop off');
-  //Sort functions 
-
-  console.log(sortedMatched)
-  for(var i =0;i < sortedMatched.length;i++){
-    //document.getElementById('empty').style.display = 'none';
-   
+  for(var i =0;i < sortedMatched.length;i++){   
     var resultsDiv = 
         `<div class="resultContainer" id="`+sortedMatched[i].text+`Results">`+ 
           `<div class="row" id="row`+sortedMatched[i].text+`">` +
@@ -78,46 +62,41 @@ function adjustPopUp(sortedMatched, color){
           `</div>`+
         `</div>`;  
 
-    
-     
+    //Check weather any tages has subtags and only inserts that tag    
+    if(sortedMatched[i].subTags.length > 0){
       var resultsBody = document.getElementById("electionPositions");
       resultsBody.insertAdjacentHTML("beforeend",resultsDiv);
-      //resultsBody.append(resultsDiv);  
-                     
+    }                
   }
    
-  //Start same as function above ^^ going run for each tag in sortedMatched
+  //creating the html for the subtags
   for(var x=0; x<sortedMatched.length;x++){
-    console.log(x)
-    //going loop through each of the subtags in each tag
     for(var y=0; y<sortedMatched[x].subTags.length;y++){
-      console.log('sortedMatched' + [x] + 'is being implemented')
-     //div that going to be returned if the subtag is greater than 1 
-      var subResultsDiv = `<div class="mini-row-hidden" id="subRow`+sortedMatched[x].subTags[y].text+`">` +
-                              `<div class="tag">` +
-                                `<p class="`+ampersandColor+` ampersand_color_mini">#</p>` +
-                                `<p class="sub-tag">`+sortedMatched[x].subTags[y].text+
-                                    `<i style='margin-left:5px;'>` +
-                                      `(`+sortedMatched[x].subTags[y].count+`)`  + 
-                                    `</i>`+
-                                `</p>` +
-                              `</div>`+   
-                              `<div class="`+buttonColor+` mini-button">`+
-                                 `<a href="`+sortedMatched[x].subTags[y].link+`" target="_blank" style='width:35px;height:35px'></a>` + 
-                              `</div>`+
-                          `</div>`;
-
+      var subResultsDiv = 
+        `<div class="mini-row-hidden" id="subRow`+sortedMatched[x].subTags[y].text+`">` +
+            `<div class="tag">` +
+              `<p class="`+ampersandColor+` ampersand_color_mini">#</p>` +
+              `<p class="sub-tag">`+sortedMatched[x].subTags[y].text+
+                  `<i style='margin-left:5px;'>` +
+                    `(`+sortedMatched[x].subTags[y].count+`)`  + 
+                  `</i>`+
+              `</p>` +
+            `</div>`+   
+            `<div class="`+buttonColor+` mini-button">`+
+               `<a href="`+sortedMatched[x].subTags[y].link+`" target="_blank" style='width:35px;height:35px'></a>` + 
+            `</div>`+
+        `</div>`;
       
-        document.getElementById(sortedMatched[x].text+`SubContainer`).insertAdjacentHTML("beforeend",subResultsDiv);
-                          
+      if(sortedMatched[x].subTags[y].count > 0 ){
+          document.getElementById(sortedMatched[x].text+`SubContainer`).insertAdjacentHTML("beforeend",subResultsDiv);
+      }                 
     }
   }
 
-console.log(sortedMatched)
+
 if (sortedMatched.length > 0) {
   var last = parseInt(sortedMatched.length);
   var lastTag = sortedMatched[last-1].text;
-  console.log(lastTag)
   document.getElementById(lastTag + "SubContainer").style.display = "none";
 
   document.getElementById(lastTag + "Results").onmouseover=function(){
