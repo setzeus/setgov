880fe142a6e511e2376c1340dd8f88198cbbe8ff
Bernardo Garciarivas <brgarciarivas@gmail.com>
2016-09-18 03:25:35 -0400

    turn my swag on

--- editPop.js
+++ editPop.js
@@ -26,104 +26,109 @@ chrome.runtime.sendMessage({action: "editPop"},function(response){
 
 //handles the data from getKeyWordsFromPage and profile change
 function adjustPopUp(sortedMatched, color){
-  //Set variables ready for color that should be displayed
+  
   if(color == 'red'){
-    var buttonColor = 'button-red';
-    var ampersandColor = 'ampersand_color_red';
-    var rowLineColor = 'row_line_red'
+    var color = "red_color";
+    var button = 'button_red';
   }
-  if(color =='blue'){
-    var buttonColor = 'button-blue';
-    var ampersandColor = 'ampersand_color_blue';
-    var rowLineColor = 'row_line_blue'
+  if(color == 'blue'){
+    var color = 'blue_color';
+    var button = 'button_blue'
   }
 
-  var clearDivv = document.getElementById('electionPositions').innerHTML = ' ' 
+  console.log(color)
+  function checkSubtag(value){
+    var clearDivv = document.getElementById('electionPositions').innerHTML = ' ' 
 
-  for(var i =0;i < sortedMatched.length;i++){   
-    var resultsDiv = 
-        `<div class="resultContainer" id="`+sortedMatched[i].text+`Results">`+ 
-          `<div class="row" id="row`+sortedMatched[i].text+`">` +
-            `<div class="tag">` +
-                `<p class="`+ampersandColor+`  ampersand_color">#</p>` +
-                `<p class="tagText`+i+` tagText">` +
-                    sortedMatched[i].text +
-                    `<i style='margin-left:5px;'>` +
-                         `(`+sortedMatched[i].count+`)`  + 
-                    `</i>`+
-                `</p>`+
-            `</div>` +
-            `<div class=" ` + buttonColor +` button primary" id="link`+i+`">` +
-                `<a href="`+sortedMatched[i].link+`" target="_blank" style='width:35px;height:35px'></a>` +
-            `</div>` +
-          `</div>` +
-          `<div class="row-line"></div>`+
-          `<div class="mini-row-container" id="`+sortedMatched[i].text+`SubContainer">`+
-          `</div>`+
-        `</div>`;  
-
-
-    
-      if( sortedMatched[i].subTags.length > 0 ){
-        var resultsBody = document.getElementById("electionPositions");
-      resultsBody.insertAdjacentHTML("beforeend",resultsDiv);
+      if(value.subTags.length > 0){
+        return value 
       }
-      
-      //resultsBody.append(resultsDiv);  
-                     
   }
-   
-  //creating the html for the subtags
-  for(var x=0; x<sortedMatched.length;x++){
-    for(var y=0; y<sortedMatched[x].subTags.length;y++){
-      var subResultsDiv = 
-        `<div class="mini-row-hidden" id="subRow`+sortedMatched[x].subTags[y].text+`">` +
-            `<div class="tag">` +
-              `<p class="`+ampersandColor+` ampersand_color_mini">#</p>` +
-              `<p class="sub-tag">`+sortedMatched[x].subTags[y].text+
-                  `<i style='margin-left:5px;'>` +
-                    `(`+sortedMatched[x].subTags[y].count+`)`  + 
-                  `</i>`+
-              `</p>` +
-            `</div>`+   
-            `<div class="`+buttonColor+` mini-button">`+
-               `<a href="`+sortedMatched[x].subTags[y].link+`" target="_blank" style='width:35px;height:35px'></a>` + 
-            `</div>`+
+  var filtered = sortedMatched.filter(checkSubtag)
+  console.log(filtered)
+
+  for(var i=0; i < filtered.length; i++){
+    console.log(i)
+    var resultDiv = 
+      `<div class="result_container" id="`+filtered[i].text+`Result">`+
+
+        `<div class="row" id="`+sortedMatched[i].texts+`Row">`+
+          `<div class='result_text'>`+
+            `<p class='ampersand `+color+`' >
+              #
+            </p>`+
+            `<p class='issue' id='tagText`+i+`'>`
+              + filtered[i].text + 
+              `<i style="margin-left:5px;">`+
+                `(`+ filtered[i].count+`)` +
+              `</i>`+
+            `</p>`+ 
+          `</div>`+
+          `<div class='button `+button+`' id='`+i+`Link'>`+
+            `<a href='`+filtered[i].link+`' target='_blank' style='width:35px;height:35px;'></a>`+
+          `</div>` +
+        `</div>`+
+        `<div class='row_status' id='row_status_`+color+`'></div>`+
+        `<div class='sub_row_container' id='`+filtered[i].text+`_sub_container'>`+
+        `</div>`+
+      `</div>`;
+
+      var resultsBody = document.getElementById("electionPositions");
+      resultsBody.insertAdjacentHTML("beforeend",resultDiv);
+
+  }
+
+  for( var x=0; x<filtered.length; x++){
+    for(var y=0;y<filtered[x].subTags.length; y++){
+      var subResultDiv = 
+        `<div class='sub_row' id="`+filtered[x].subTags[y].text+`SubRow">`+
+          `<div class='sub_result_text' style="margin-left:15px;">`+
+            `<p class='sub_amperand `+color+`'>#</p>`+
+            `<p class='sub_text'>`+filtered[x].subTags[y].text+
+                `<i style='margin-left:5px;'>`+
+                  `(`+filtered[x].subTags[y].count+`)` + 
+                `</i>`+
+            `</p>` +
+          `</div>`+   
+            `<div class='sub_button `+button+`'>`+
+              `<a href='`+filtered[x].subTags[y].link+ `' target='_blank' style='width:35px;height:35px'></a>`+
+            `</div>`+    
+
+          `</div>`+
         `</div>`;
-      
-      if(sortedMatched[x].subTags[y].count > 0 ){
-          console.log('check to see if any subs tags were attempted to be inputed')
-          document.getElementById(sortedMatched[x].text+`SubContainer`).insertAdjacentHTML("beforeend",subResultsDiv);
-      }                 
+
+        document.getElementById(filtered[x].text+`_sub_container`).insertAdjacentHTML("beforeend", subResultDiv);
     }
   }
 
+  // if (filtered.length > 0) {
+  //   var last = parseInt(filtered.length);
+  //   var lastTag = filtered[last-1].text;
+  //   console.log(lastTag)
+  //   document.getElementById(lastTag + "_sub_container").style.display = "none";
 
-console.log(sortedMatched)
-if (sortedMatched.length > 0) {
-  var last = parseInt(sortedMatched.length);
-  console.log(last)
-  var lastTag = sortedMatched[last-1].text;
-  console.log(lastTag)
-  document.getElementById(lastTag + "SubContainer").style.display = "none";
+  //   document.getElementById(lastTag + "Result").onmouseover=function(){
+  //     document.getElementById(lastTag + "_sub_container").style.display = "flex";
+  //     };
 
-  document.getElementById(lastTag + "Results").onmouseover=function(){
-    document.getElementById(lastTag + "SubContainer").style.display = "flex";
-    };
+  //   document.getElementById(lastTag + "Result").onmouseleave=function(){
+  //     document.getElementById(lastTag + "_sub_container").style.display = "none";
+  //   };
 
-  document.getElementById(lastTag + "Results").onmouseleave=function(){
-    document.getElementById(lastTag + "SubContainer").style.display = "none";
-  };
+  //   document.getElementById(lastTag + "_sub_container").onmouseleave=function(){
+  //     document.getElementById(lastTag + "_sub_container").style.display = "flex";
+  //   };
 
-  document.getElementById(lastTag + "SubContainer").onmouseleave=function(){
-    document.getElementById(lastTag + "SubContainer").style.display = "none";
-  };
-
-  console.log(last);
-  console.log(lastTag);
-};
-
-
-
-
+  //   console.log(last);
+  //   console.log(lastTag);
+  // };
 }
+
+
+
+
+
+
+
+
+
