fbb4c93125367222aa24cee0df0f4a11f89c779c
sinnott-balin <sinnott.balin@gmail.com>
2016-10-07 15:54:08 -0400

    first react2.0 commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const App = React.createClass({
+ 
+  render: function () {
+    return (
+     <div className="scoreboard">
+     	<div className="header">
+      		<h1>App</h1>
+    	</div>
+     </div>
+    );
+  }
+});
+
+export default App;
