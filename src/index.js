0b879f0b036eedd3d8515e7293fed606135c00df
sinnott-balin <sinnott.balin@gmail.com>
2016-10-09 13:03:05 -0400

    removing files

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App.js';
-
-import './css/style.css'
-
-ReactDOM.render(<App />, document.getElementById('container'));
