5b7ca14918a5e6e1e7707033bd704a727f277eff
sinnott-balin <sinnott.balin@gmail.com>
2016-10-09 09:41:28 -0400

    adding files

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.js';
+
+import './css/style.css'
+
+ReactDOM.render(<App />, document.getElementById('container'));
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require('path');
+//var buildPath = path.resolve(__dirname, 'src');
+
+module.exports = {
+
+	entry: './src/index.js',
+
+	output: {
+		path: 'src/bundles',
+		filename: 'app_bundle.js'
+	},
+
+	devServer: {
+		contentBase: "./src",
+		hot: true,
+		inline: true
+	},
+
+	module: {
+		loaders: [
+
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			},
+			{
+				test: /\.css$/,
+				loader: 'style-loader!css-loader'
+			}
+		],
+		
+	}
+
+};
