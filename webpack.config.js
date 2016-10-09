bb93e384655442647b393927f721b83a8e1dc0f4
sinnott-balin <sinnott.balin@gmail.com>
2016-10-09 17:04:13 -0400

    updating webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,9 @@
-var path = require('path');
-var buildPath = path.resolve(__dirname, 'bundles');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var webpack = require('webpack');
-
-
-
 module.exports = {
 
 	entry: './src/index.js',
 	
 	output: {
-		path: buildPath,
+		path: 'bundles',
 		filename: 'app_bundle.js',
 		publicPath: '/bundles'
 	},
