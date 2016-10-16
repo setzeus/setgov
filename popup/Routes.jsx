390dd25596713351b846392d0a4b5aac4ba37843
sinnott-balin <sinnott.balin@gmail.com>
2016-10-16 12:53:09 -0400

    updating routes

--- popup/Routes.jsx
+++ popup/Routes.jsx
@@ -3,11 +3,13 @@ import Route from 'react-router/lib/Route';
 import IndexRoute from 'react-router/lib/IndexRoute';
 
 import App from './components/App';
+import CandidateListPage from './components/CandidateListPage';
 
 export default(
 
     <Route path='/index.html'>
         <IndexRoute component={App}/>
+        <Route path='test' component={CandidateListPage}/>
     </Route>
 
 );
