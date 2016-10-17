3ca0a4328b3459d7a3de9052a232c9323f33184d
Oscar Lafarga <otech47@gmail.com>
2016-10-17 10:00:20 -0400

    reimplementing react router

---
+++ popup/Routes.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Route from 'react-router/lib/Route';
+
+import App from './components/App';
+
+export default(
+
+    <Route path='/index.html' component={App} />
+
+);
